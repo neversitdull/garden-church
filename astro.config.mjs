b85860bf09cc74b9cfb776aa311024895a081d9b
Josh Dunsterville <neversitdull@icloud.com>
2025-05-18 20:50:48 -0700

    Add fonts and color styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config"
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google({
+          experimental: {
+            variableAxis: {
+              Archivo: { wdth: [["62.5", "125"]] },
+            },
+          },
+        }),
+        name: "Archivo",
+        cssVariable: "--font-archivo",
+        weights: ["100 900"],
+        styles: ["normal", "italic"],
+        subsets: ["latin"],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Geist",
+        cssVariable: "--font-geist",
+        weights: ["100 900"],
+        styles: ["normal"],
+        subsets: ["latin"],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Geist Mono",
+        cssVariable: "--font-geist-mono",
+        weights: ["100 900"],
+        styles: ["normal"],
+        subsets: ["latin"],
+      },
+    ],
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+})
