e6e2d0ae6541d3658bd01c8a175ec6e0f40ee68b
Josh Dunsterville <neversitdull@icloud.com>
2025-06-03 12:56:57 -0700

    Remove nav config for now

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -27,33 +27,3 @@ export const services = {
     times: ["TUE 7PM", "WED 7:30AM", "THU 9AM"],
   },
 };
-
-export const mainNav = [
-  { label: "Home", path: "/" },
-  { label: "Sundays", path: "/sundays" },
-  {
-    label: "Ministries",
-    ministries: [
-      { label: "Youth", path: "/ministries/youth" },
-      { label: "Kids", path: "/ministries/kids" },
-      { label: "House Church", path: "/ministries/house-church" },
-      { label: "Prayer", path: "/ministries/prayer" },
-      { label: "Global Outreach", path: "/ministries/global" },
-      { label: "Local Outreach", path: "/ministries/local" },
-      { label: "All Ministries", path: "/ministries" },
-    ],
-  },
-  {
-    label: "Events",
-    path: "https://gardenchurch.churchcenter.com/registrations",
-  },
-  { label: "Serve", path: "/serve" },
-  { label: "Give", path: "/give" },
-];
-
-export const footerNav = [
-  { label: "Home", path: "/" },
-  { label: "About", path: "/about" },
-  { label: "Services", path: "/services" },
-  { label: "Contact", path: "/contact" },
-];
