d05804bf361e01680601b18b7936756579f711f7
Josh Dunsterville <neversitdull@icloud.com>
2025-06-05 14:06:16 -0700

    Add logo

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,12 +1,14 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import { Logo } from "./components/logo";
 import { schemaTypes } from "./schemaTypes";
 import { structure } from "./structure";
 
 export default defineConfig({
   name: "default",
-  title: "garden-church",
+  title: "Garden Church",
+  icon: Logo,
 
   projectId: "k4pfgwt6",
   dataset: "production",
