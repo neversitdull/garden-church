c1e939afb50eccfbdbf52800f52d6d8641fcd10c
Josh Dunsterville <neversitdull@icloud.com>
2025-06-06 15:27:28 -0700

    Remove Sanity Studio and related configurations from the project

--- studio/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import { navigationType } from "./documents/navigationType";
-import { pageType } from "./documents/pageType";
-import { linkType } from "./objects/linkType";
-
-export const schemaTypes = [navigationType, pageType, linkType];
--- web/src/lib/sanityClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const sanityClient = createClient({
-  projectId: "k4pfgwt6",
-  dataset: "production",
-  useCdn: false,
-  apiVersion: "2023-05-03",
-});
